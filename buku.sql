e6c38b41647aec28678daba29b66f7042053abe5
Wirapermana <wira.pendit@gmail.com>
2025-12-01 20:21:29 +0800

    Menambahkan file schema database

---
+++ buku.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE buku;
+USE buku;
+
+CREATE TABLE buku (
+    id_buku INT AUTO_INCREMENT PRIMARY KEY,
+    judul VARCHAR(150) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    kategori VARCHAR(50) NOT NULL,
+    tahun_terbit INT NOT NULL,
+    status ENUM('tersedia','dipinjam') NOT NULL,
+    cover VARCHAR(255)
+);
